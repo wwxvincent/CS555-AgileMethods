4f2b1fb28d701ef358a706afbe2a486ef57d3f7a
Shweta Singh <60408620+ssingh59@users.noreply.github.com>
2020-02-15 23:18:57 -0500

    added my surname- testing github

--- Individual.java
+++ Individual.java
@@ -1,6 +1,6 @@
 package project3;
 //Author: Wenxuan Wang
-//Team Members: Wenxuan; Chengyi; Shweta
+//Team Members: Wenxuan; Chengyi; Shweta singh
 //purpose: for CS555-project3:
 //Date: 2/13/2020
 //description: sub class:Individual for GedcomParse;
