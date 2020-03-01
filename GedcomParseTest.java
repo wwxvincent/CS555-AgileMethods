8ef66677edd0d3775f85fcea2d4d186d4f8180d9
Shweta Singh <60408620+ssingh59@users.noreply.github.com>
2020-02-29 19:49:10 -0500

    added test file

---
+++ GedcomParseTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GedcomParseTest {
+
+	@Test
+	public void testGedcomParse() {
+		//fail("Not yet implemented");
+		GedcomParse test = new GedcomParse();
+		
+	}
+
+}
